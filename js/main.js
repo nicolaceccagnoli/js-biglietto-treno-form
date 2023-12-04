a09e881a9b02c27abe0db860d05fe1bd8c86d0ee
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-04 15:02:25 +0100

    Add point 1

--- js/main.js
+++ js/main.js
@@ -7,3 +7,39 @@
 
 */
 
+// Seleziono il Bottone dall' HTML
+
+const myButton = document.querySelector('#special-button');
+
+// Creo la Variabile che conterà il numero dei 'click' sul bottone del mio Form
+
+let counter = 0;
+
+// Imposto l'evento che viene scatenato dal 'click' su "myButton"  
+
+myButton.addEventListener('click', function() {
+
+
+//  Verifico che l'evento venga richiamato correttamente
+
+    counter += 1;
+
+    console.log('Hai inviato il Form ' + counter + ' volte');
+
+    // 1) Chiedi all' Utente il numero di chilometri da percorrere
+
+    const kmInput = document.getElementById('km');
+
+    console.log('kmInput', kmInput, typeof kmInput);
+    console.log('kmInput.value', kmInput.value , typeof kmInput.value);
+
+});
+
+/*
+
+
+    const ageInput = document.getElementById('age');
+    
+    console.log('ageInput', ageInput, typeof ageInput);
+
+*/
