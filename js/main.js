8259e658dac3e6db9a2b07e3d4f16391ebf88132
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-04 13:11:55 +0100

    Impostato Esercizio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* 
+
+1) Chiedi all' Utente il numero di chilometri da percorrere
+2) Chiedi all' Utente l'età
+3) Applica lo sconto in base all'età del passeggero
+4) Stampo il risultato in pagina
+
+*/
